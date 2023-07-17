77f36cff19f94fa0e1de6f6a71c60b56f3a904bc
Kyuwoong Lee <kwooler@gmail.com>
2023-07-16 22:48:08 -0700

    Initial commit

---
+++ main.py
@@ -0,0 +1,21 @@
+def insertOne(numbers, val):
+    """
+    ########################################
+    Code Your Program here
+    Do not use 'sort' keyword
+    ########################################
+    """
+
+
+def main():
+    numbers = [5, 20, 30, 35, 50]
+    print(f'The original list value: {numbers}')
+    insertOne(numbers, 25)
+    print(f'After insertion 25: {numbers}')
+
+
+##
+
+
+if __name__ == '__main__':
+    main()
